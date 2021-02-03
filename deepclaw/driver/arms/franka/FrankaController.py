2ddbe2b1664da3b0348f919e43b971995a65efcb
lxb1989 <42053289+lxb1989@users.noreply.github.com>
2021-02-03 15:14:33 +0800

    update franka driver

--- deepclaw/driver/arms/franka/FrankaController.py
+++ deepclaw/driver/arms/franka/FrankaController.py
@@ -3,18 +3,41 @@ import time
 import sys
 import os
 
-ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
 sys.path.append(ROOT)
 
 from deepclaw.driver.arms.ArmController import ArmController
 from deepclaw.driver.arms.URConnector import URConnector
 
+
 import yaml
 import numpy as np
 from scipy.spatial.transform import Rotation as RR
-
-
 import RobotDriver
+
+robot_state = {'Joints': [],  # Actual joint positions
+               'Joints_Velocity': [],  # Actual joint velocities
+               'Joints_Current': [],  # Actual joint currents
+               'Joints_Voltage': [],  # Actual joint voltages
+               'Joints_Temperature': [],  # Temperature of each joint in degrees Celsius
+               'Joints_Target': [],  # Target joint positions
+               'Joints_Velocity_Target': [],  # Target joint velocities
+               'Joints_Acceleration_Target': [],  # Target joint accelerations
+               'Joints_Current_Target': [],  # Target joint currents
+               'Joints_Torque_Target': [],  # Target joint moments (torques)
+               'Joints_Current_Control': [],  # Joint control currents
+               'TCP_Pose': [],  # Actual Cartesian coordinates of the tool: (x,y,z,rx,ry,rz)
+               'TCP_Force': [],  # Generalized forces in the TCP
+               'TCP_Velocity': [],  # Actual speed of the tool given in Cartesian coordinates
+               'TCP_Acceleration': [],  # Tool x, y and z accelerometer values
+               'TCP_Pose_Target': [],  # Target Cartesian coordinates of the tool: (x,y,z,rx,ry,rz)
+               'TCP_Velocity_Target': [],  # Target speed of the tool given in Cartesian coordinates
+               'Speed_Fraction_Target': [],  # Target speed fraction, running speed fraction in teach pad
+               'Speed_Scaling': [],  # Speed scaling of the trajectory limiter
+               'Momentum': [],  # Norm of Cartesian linear momentum
+               'isProtectiveStopped': []  # a bool indicating if the robot is in ‘Protective stop’
+               }
+
 
 class FrankaController(ArmController):
     def __init__(self, robot_configuration_file_path):
@@ -34,13 +57,13 @@ class FrankaController(ArmController):
         #O_T_EE: 4*4 Matrix from Base to EE
         O_T_EE = np.array(self.fk.getO_T_EE())
         O_Matrix = np.reshape(O_T_EE,(4,4),order='F')
-        O_rotation = RR.from_matrix(O_Matrix)
+        O_rotation = RR.from_matrix(O_Matrix[0:3, 0:3])
         O_euler = O_rotation.as_euler('xyz', degrees=False)
 
         # #F_T_EE: 4*4 Matrix from Flange to EE
         # F_T_EE = np.array(self.fk.getF_T_EE())
         # F_Matrix = np.reshape(F_T_EE,(4,4),order='F')
-        return O_euler
+        return O_Matrix[0:3, 3], O_euler
 
     def getJoint(self):
         return self.fk.getJointPos()
@@ -48,7 +71,19 @@ class FrankaController(ArmController):
     def getState(self):
         current_state = self.fk.getAllState()
         current_state = eval(current_state)
-        return current_state
+        
+        robot_state["Joints"] = current_state['q']
+        robot_state["Joints_Velocity"] = current_state['dq']
+        robot_state["Joints_Acceleration_Target"] = current_state['ddq_d']
+        
+        temp = current_state['O_T_EE']
+        O_Matrix = np.reshape(temp,(4,4),order='F')
+        O_rotation = RR.from_matrix(O_Matrix[0:3, 0:3])
+        O_euler = O_rotation.as_euler('xyz', degrees=False)
+        xyz = O_Matrix[0:3, 3]
+        robot_state["TCP_Pose"] = [xyz[0], xyz[1], xyz[2], O_euler[0], O_euler[1], O_euler[2]]
+
+        return robot_state
 
     def gripperOpen(self):
         self.fk.gripperOpen()
@@ -95,12 +130,11 @@ class FrankaController(ArmController):
         self.fk.setJointPos(target_joint)
 
 if __name__ == '__main__':
-    FC = FrankaController(ROOT + '/config/robcell-panda1-default-d435/'+'franka.yaml')
+    FC = FrankaController(ROOT + '/configs/robcell-panda1-default-d435/'+'franka.yaml')
     print(FC.getJoint())
-    print(FC.getCartesianPose())
+    #print(FC.getCartesianPose())
     allState = FC.getState()
-    print(allState['F_T_EE'])
-    print(allState['O_T_EE'])
+    print('state: ', allState)
 
     joint_target = np.array([-0.0137566,0.0150639,0.06416,-2.50988,-0.00736516,2.80153,-1.8411])
     FC.move_j(joint_target)
@@ -111,6 +145,7 @@ if __name__ == '__main__':
     FC.speed_j(joint_speed)
     time.sleep(2)
     FC.stopSpeed()
+    print('===========')
 
     FC.move_p([0.5,0,0.3,3.14,0.0,0.0])
     FC.move_p([0.6,0,0.4,3.14,0.0,1.0])
--- deepclaw/driver/arms/franka/RobotDirver/Readme.md
+++ deepclaw/driver/arms/franka/RobotDirver/Readme.md
@@ -4,7 +4,8 @@ This driver is implement with libfranka and pybind11, and provides python3 API.
 # How to use
 Before compiling the files, make sure you have install [pybind11](https://github.com/pybind/pybind11), [libfranka](https://frankaemika.github.io/docs/installation_linux.html), [EIGEN3](http://eigen.tuxfamily.org/index.php?title=Main_Page), and libpoco-dev.
 
-Then locate the RobotDirver, open the terminal, and type commands as below:
+Then locate the RobotDirver, open the terminal, transfer to the python environment you used,
+and type commands as below:
 ```
 mkdir build
 cd build
@@ -12,3 +13,5 @@ cmake ..
 make -j4
 cp RobotDriver.cpython-35m-x86_64-linux-gnu.so ../../RobotDriver.cpython-35m-x86_64-linux-gnu.so
 ```
+Note: as the python version is 3.5, the library name is '**cpython-35m**', 
+for other version, the name will be changed.
